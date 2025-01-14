2cd211e6e8aff8c6cdf4d9ee20ade3a8e0073af5
medooon <167296507+medooon@users.noreply.github.com>
2025-01-14 18:39:58 +0200

    Update offline_manager.dart

--- lib/utils/offline_manager.dart
+++ lib/utils/offline_manager.dart
@@ -11,4 +11,35 @@ class OfflineManager {
     prefs.setString(_lastFetchKey, date.toIso8601String());
   }
 
-  // Get
+  // Get the last fetch date
+  static Future<DateTime?> getLastFetchDate() async {
+    final prefs = await SharedPreferences.getInstance();
+    final dateString = prefs.getString(_lastFetchKey);
+    return dateString != null ? DateTime.parse(dateString) : null;
+  }
+
+  // Set the number of offline skips
+  static Future<void> setOfflineSkips(int skips) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt(_offlineSkipKey, skips);
+  }
+
+  // Get the remaining offline skips
+  static Future<int> getOfflineSkips() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_offlineSkipKey) ?? _maxSkips;
+  }
+
+  // Decrement offline skips
+  static Future<void> decrementOfflineSkips() async {
+    final currentSkips = await getOfflineSkips();
+    if (currentSkips > 0) {
+      await setOfflineSkips(currentSkips - 1);
+    }
+  }
+
+  // Reset offline skips to max value
+  static Future<void> resetOfflineSkips() async {
+    await setOfflineSkips(_maxSkips);
+  }
+}
