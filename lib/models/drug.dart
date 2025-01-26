2749818a01589b208ecd7f0ecde3296743c3d6e7
medooon <167296507+medooon@users.noreply.github.com>
2025-01-26 16:55:23 +0200

    Update drug.dart

--- lib/models/drug.dart
+++ lib/models/drug.dart
@@ -1,5 +1,3 @@
-// lib/models/drug.dart
-
 import 'package:hive/hive.dart';
 
 part 'drug.g.dart'; // This will be generated automatically
@@ -7,30 +5,53 @@ part 'drug.g.dart'; // This will be generated automatically
 @HiveType(typeId: 0)
 class Drug extends HiveObject {
   @HiveField(0)
-  final String tradeName;
+  final String id;
 
   @HiveField(1)
-  final String genericName;
+  final String ke;
 
   @HiveField(2)
-  final String pharmacology;
+  final String tradeName;
 
   @HiveField(3)
-  final String arabicName;
+  final String genericName;
 
   @HiveField(4)
-  final double price;
+  final String pharmacology;
 
   @HiveField(5)
-  final String company;
+  final String arabicName;
 
   @HiveField(6)
-  final String description;
+  final double price;
 
   @HiveField(7)
+  final String company;
+
+  @HiveField(8)
+  final String description;
+
+  @HiveField(9)
   final String route;
 
+  @HiveField(10)
+  final String temperature;
+
+  @HiveField(11)
+  final String otc;
+
+  @HiveField(12)
+  final String pharmacy;
+
+  @HiveField(13)
+  final String descriptionId;
+
+  @HiveField(14)
+  final bool isCalculated;
+
   Drug({
+    required this.id,
+    required this.ke,
     required this.tradeName,
     required this.genericName,
     required this.pharmacology,
@@ -39,19 +60,30 @@ class Drug extends HiveObject {
     required this.company,
     required this.description,
     required this.route,
+    required this.temperature,
+    required this.otc,
+    required this.pharmacy,
+    required this.descriptionId,
+    required this.isCalculated,
   });
 
-factory Drug.fromJson(Map<String, dynamic> json) {
-  return Drug(
-    tradeName: json['trade_name'] ?? 'N/A',
-    genericName: json['generic_name'] ?? 'N/A',
-    pharmacology: json['pharmacology'] ?? 'N/A',
-    arabicName: json['arabic_name'] ?? 'N/A',
-    price: json['price'] != null ? (json['price'] as num).toDouble() : 0.0,
-    company: json['company'] ?? 'N/A',
-    description: json['description'] ?? 'No description available.',
-    route: json['route'] ?? 'N/A',
-  );
- }
+  factory Drug.fromJson(Map<String, dynamic> json) {
+    return Drug(
+      id: json['id'] ?? 'N/A',
+      ke: json['ke'] ?? 'N/A',
+      tradeName: json['trade_name'] ?? 'N/A',
+      genericName: json['generic_name'] ?? 'N/A',
+      pharmacology: json['pharmacology'] ?? 'N/A',
+      arabicName: json['arabic'] ?? 'N/A',
+      price: json['price'] != null ? (json['price'] as num).toDouble() : 0.0,
+      company: json['company'] ?? 'N/A',
+      description: json['description'] ?? 'No description available.',
+      route: json['route'] ?? 'N/A',
+      temperature: json['temperature'] ?? 'N/A',
+      otc: json['otc'] ?? 'N/A',
+      pharmacy: json['pharmacy'] ?? 'N/A',
+      descriptionId: json['description_id'] ?? 'N/A',
+      isCalculated: json['is_calculated'] ?? false,
+    );
+  }
 }
-
